a26e91961928219c8c7c02ddd3a5f450ba42cda0
japrietov <japrietov@unal.edu.co>
2016-09-13 19:59:41 -0500

    Encryption Works.

--- DES_implementation.py
+++ DES_implementation.py
@@ -55,6 +55,20 @@ def PC_1(key):
 def shiftLbyn(arr, n):
     return arr[n::] + arr[:n:]
 
+# Permutation P
+def permutation_P(f_inner):
+    matrix_f_inner = [[16, 7, 20, 21, 29, 12, 28, 17],
+                      [1, 15, 23, 26, 5, 18, 31, 10],
+                      [2, 8, 24, 14, 32, 27, 3, 9],
+                      [19, 13, 30, 6, 22, 11, 4, 25]]
+
+    p_out = []
+    for rows in matrix_f_inner:
+        for cols in rows:
+            p_out.append(f_inner[cols - 1])
+
+    return "".join(p_out)
+
 # PC - 2,
 def PC_2(key_Ci_Di):
     permutation_matrix = [[14, 17, 11, 24, 1, 5],
@@ -119,6 +133,129 @@ def initial_permutation(message):
     return "".join(message_prime)
 
 
+# Final permutation IP-1
+def final_permutation(l_last, r_last):
+    matrix_final_permutation = [[40, 8, 48, 16, 56, 24, 64, 32],
+                                [39, 7, 47, 15, 55, 23, 63, 31],
+                                [38, 6, 46, 14, 54, 22, 62, 30],
+                                [37, 5, 45, 13, 53, 21, 61, 29],
+                                [36, 4, 44, 12, 52, 20, 60, 28],
+                                [35, 3, 43, 11, 51, 19, 59, 27],
+                                [34, 2, 42, 10, 50, 18, 58, 26],
+                                [33, 1, 41, 9, 49, 17, 57, 25]]
+
+
+    message_final = r_last + l_last
+    cipher_text = []
+    for row in matrix_final_permutation:
+        for col in row:
+            cipher_text.append(message_final[col-1])
+
+    return "".join(cipher_text)
+
+
+
+# Expansion Function E
+def expansion(right_part):
+    matrix_expansion = [[32, 1, 2, 3, 4, 5],
+                        [4, 5, 6, 7, 8, 9],
+                        [8, 9, 10, 11, 12, 13],
+                        [12, 13, 14, 15, 16, 17],
+                        [16, 17, 18, 19, 20, 21],
+                        [20, 21, 22, 23, 24, 25],
+                        [24, 25, 26, 27, 28, 29],
+                        [28, 29, 30, 31, 32, 1]]
+
+    right_expanded = []
+    for row in matrix_expansion:
+        for col in row:
+            right_expanded.append(right_part[col-1])
+
+    return "".join(right_expanded)
+
+# S-box
+def S_box(B_i, i_box):
+    row_index = int(B_i[0]+B_i[5],2)
+    col_index = int(B_i[1:5],2)
+
+    sBox = []
+
+    sBox.append([[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+                 [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+                 [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+                 [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]])
+
+    sBox.append([[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+                 [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+                 [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+                 [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]])
+
+    sBox.append([[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+                 [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+                 [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+                 [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]])
+
+    sBox.append([[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+                 [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+                 [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+                 [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]])
+
+    sBox.append([[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+                 [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+                 [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+                 [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]])
+
+    sBox.append([[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+                 [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+                 [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+                 [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]])
+
+    sBox.append([[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+                 [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+                 [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+                 [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]])
+
+    sBox.append([[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+                 [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+                 [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+                 [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]])
+
+    value = bin(sBox[i_box][row_index][col_index])[2:]
+    if len(value) < 4: value = list(repeat("0", 4 - len(value))) + list(value)
+    return "".join(value)
+
+# Compute R_i, L_i
+def compute_next_R_L(left_0, right_0, key_dict):
+    dict_Li = {}
+    dict_Ri = {}
+    current_L_i = left_0
+    current_R_i = right_0
+
+    for index in xrange(1, 17):
+        dict_Li[index] = current_R_i
+        tmp_solution_Ri = bin(int(current_L_i, 2) ^ int(inner_function(current_R_i, key_dict[index]),2))[2:]
+        if len(tmp_solution_Ri) < 32: tmp_solution_Ri = list(repeat("0", 32 - len(tmp_solution_Ri))) + list(tmp_solution_Ri)
+        dict_Ri[index] = "".join(tmp_solution_Ri)
+
+        current_L_i = dict_Li[index]
+        current_R_i = dict_Ri[index]
+
+    return dict_Li, dict_Ri
+
+# Inner function f
+def inner_function(right_part, key_i):
+    right_expansion = expansion(right_part)
+    k_xor_right =  bin(int(right_expansion,2) ^ int(key_i,2))[2:]
+    if len(k_xor_right) < 48: k_xor_right = list(repeat("0", 48 - len(k_xor_right))) + list(k_xor_right)
+
+    B_i_list  =re.findall('......', "".join(k_xor_right))
+
+    B_list_S_box = []
+    for i in xrange(8):
+        B_list_S_box.append(S_box(B_i_list[i], i))
+
+    return permutation_P("".join(B_list_S_box))
+
 
 #####################
 # NO ELIMINAR!!
@@ -130,10 +267,16 @@ key_test = "".join([chr(19), chr(52), chr(87), chr(121), chr(155), chr(188), chr
 test_key_prime = "11110000110011001010101011110101010101100110011110001111"
 
 tmp = key_generator(key_test)
-#for i in tmp:
-#    print tmp[i]
 
-print initial_permutation(message_test)
+dic_l, dic_r = compute_next_R_L(initial_permutation(message_test)[:len(message_test)/2], initial_permutation(message_test)[len(message_test)/2:], tmp)
+
+
+cipher_text = final_permutation(dic_l[16], dic_r[16])
+
+# Print the cipher text in a list of 8 bytes
+print re.findall('........', "".join(cipher_text))
+
+
 
 
 
